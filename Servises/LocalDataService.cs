276d72a2ffa734caf7b4370dd64c6cc0b2daaed5
Sigmuchest228 <alimon3000@gmail.com>
2025-12-24 15:31:54 +0200

    dataBase delayem

--- Servises/LocalDataService.cs
+++ Servises/LocalDataService.cs
@@ -4,22 +4,58 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using SigmaChess.Models;
+using Firebase.Auth;
+using Firebase.Auth.Providers;
+using Firebase.Auth.Repository;
+using Firebase.Database;
+using Firebase.Database.Query;
+
+// name collision resolve
+using FirebaseUser = Firebase.Auth.User;
+using User = SigmaChess.Models.User;
 
 namespace SigmaChess.Servises
 {
     class LocalDataService
     {
+        static FirebaseAuthClient? auth;
+        static FirebaseClient? client;
+        static public AuthCredential? loginAuthUser;
+
         #region instance
         private static LocalDataService instance;
         public LocalDataService()
         {
-            CreateFakeData();
+            //CreateFakeData();
         }
+        public void InitAuth()
+        {
+            var config = new FirebaseAuthConfig()
+            {
+                ApiKey = "AIzaSyDLgBH5NwO6_aIuW5sZHnSrip78bOih2fk",
+                AuthDomain = "sigmachess-75f04.firebaseapp.com", //כתובת התחברות
+                Providers = new FirebaseAuthProvider[] //רשימת אפשריות להתחבר
+              {
+          new EmailProvider() //אנחנו נשתמש בשירות חינמי של התחברות עם מייל
+              },
+                UserRepository = new FileUserRepository("appUserData") //לא חובה, שם של קובץ בטלפון הפרטי שאפשר לשמור בו את מזהה ההתחברות כדי לא הכניס כל פעם את הסיסמא 
+            };
+            auth = new FirebaseAuthClient(config); //ההתחברות
+
+            client =
+              new FirebaseClient(@"https://sigmachess-75f04-default-rtdb.europe-west1.firebasedatabase.app/", //כתובת מסד הנתונים
+              new FirebaseOptions
+              {
+                  AuthTokenAsyncFactory = () => Task.FromResult(auth.User.Credential.IdToken)// מזהה ההתחברות של המשתמש עם השרת, הנתון נשמר במכשיר
+              });
+        }
+
         public static LocalDataService GetInstance()
         {
             if (instance == null)
             {
                 instance = new LocalDataService();
+                instance.InitAuth();
             }
             return instance;
         }
